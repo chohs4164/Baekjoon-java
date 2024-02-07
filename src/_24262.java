7c09c09f2fbcad849efae3af35939bbc8ffe4873
chohs4164 <138971722+chohs4164@users.noreply.github.com>
2024-02-07 14:36:49 +0900

    Add files via upload

---
+++ src/_24262.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _24262 {
+//    public int MenOfPassion(A[],n){
+//        int i = [n/2];
+//        return A[i];
+//    }
+    public static void main(String[] args) {
+        //MenOfPassion함수는 n이 아무리 커진다고 해도 수행횟수는 1이므로
+        //시간 복잡도는 O(1)이고 1이므로 최고차항의 차수는 항상 0이다.
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
