25ac9b134504e250b5f7acae0a62dca0f0bfdd33
chohs4164 <138971722+chohs4164@users.noreply.github.com>
2024-02-09 13:27:46 +0900

    Add files via upload

---
+++ src/_24266.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _24266 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n=scanner.nextLong();
+        System.out.println(n*n*n);
+        System.out.println(3);
+    }
+}
