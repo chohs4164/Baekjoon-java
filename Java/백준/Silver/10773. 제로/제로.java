bd40e9fecf69e125f26ae39f1e44fc614d1ec362
Hyun Seung Cho <josh100as@gmail.com>
2026-03-17 16:35:14 +0900

    [Silver IV] Title: 제로, Time: 768 ms, Memory: 108528 KB -BaekjoonHub

--- Java/백준/Silver/10773. 제로/제로.java
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -1,10 +1,12 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<Integer>();
+        Deque<Integer> stack = new ArrayDeque<Integer>();
         int sum=0;
         int K = sc.nextInt();
         // 스택에 돈 액수 저장
