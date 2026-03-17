a99787f4dafc6ac170967606ae406d8eb6044868
Hyun Seung Cho <josh100as@gmail.com>
2026-03-17 15:53:29 +0900

    [Silver IV] Title: 제로, Time: 708 ms, Memory: 108856 KB -BaekjoonHub

---
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<Integer>();
+        int sum=0;
+        int K = sc.nextInt();
+        // 스택에 돈 액수 저장
+        for (int i = 0; i < K; i++) {
+           int num = sc.nextInt();
+           if(num == 0)
+               stack.pop();
+           else
+               stack.push(num);
+        }
+        // 하나씩 빼면서 합 하기
+        while(!stack.isEmpty()){
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}
