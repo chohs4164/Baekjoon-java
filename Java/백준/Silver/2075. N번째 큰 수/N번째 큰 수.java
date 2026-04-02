e2181aa9aaa4495889fba6f4cec5ac90d9a4ccaf
Hyun Seung Cho <josh100as@gmail.com>
2026-04-02 14:51:05 +0900

    [Silver III] Title: N번째 큰 수, Time: 808 ms, Memory: 276632 KB -BaekjoonHub

---
+++ Java/백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        PriorityQueue pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for (int j = 1; j < N; j++) {
+            pq.poll();
+        }
+        sb.append(pq.poll());
+        System.out.println(sb);
+    }
+}
